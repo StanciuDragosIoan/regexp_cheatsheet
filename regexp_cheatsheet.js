16fd22a95c4996574dfbc5386fa7d289cd6a1a57
Stanciu Dragoș Ioan <39739297+StanciuDragosIoan@users.noreply.github.com>
2020-01-28 09:47:15 +0200

    Create regexp_cheatsheet.js

---
+++ regexp_cheatsheet.js
@@ -0,0 +1,17 @@
+/*
+
+find all .c files => find -regex '.*/.*\.\(c\|cpp\|h\)$'   
+find all .js files => find -regex '.*/.*\.\(js\|cpp\|h\)$'
+fint all .php files => find -regex '.*/.*\.\(php\|cpp\|h\)$'
+
+etc...
+
+
+
+
+
+
+
+
+
+*/
